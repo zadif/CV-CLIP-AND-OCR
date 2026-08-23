e4784c47578161428a2284d248fa029547d8ca75
Zadif <2023cs608@student.uet.edu.pk>
2026-08-23 17:07:30 +0500

    Added qdrant to store embeddings in it

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ import time
 import watchdog.events
 import watchdog.observers
 import os
+from qdrant_client import QdrantClient
+from qdrant_client.models import Distance, VectorParams, PointStruct, FilterSelector, Filter, FieldCondition, MatchValue
+import uuid
+import hashlib
 
 
 reader = easyocr.Reader(['en'], gpu=False) 
@@ -14,22 +18,37 @@ openclipModel= 'ViT-B-32'
 model, _, preprocess = open_clip.create_model_and_transforms(openclipModel, pretrained='openai')
 model.eval() 
 
-DEBUG=False
-database = []
+DEBUG=True
 
+# Setting up the qdrant client
+collectionName="images"
+qdrant=QdrantClient(path="./qdrant")
+if not (qdrant.collection_exists(collection_name=collectionName)):
+    qdrant.create_collection(
+        collection_name=collectionName,
+        vectors_config=VectorParams(
+            size=512,
+            distance=Distance.COSINE
+        )
+    )
+
+namespace=uuid.NAMESPACE_DNS
 
 def extractTextFromImage(image: str) -> list[str]:
+    try:
+        if not image:
+            raise ValueError("Image path cannot be empty.")
 
-    if not image:
-        raise ValueError("Image path cannot be empty.")
+        "Detail=0 make sure we dont get bounding box in output"
 
-    "Detail=0 make sure we dont get bounding box in output"
-
-    result = reader.readtext(image, detail=0)
-    if result is None:
-        raise ValueError("OCR failed")
-    
-    return result
+        result = reader.readtext(image, detail=0)
+        if result is None:
+            raise ValueError("OCR failed")
+        
+        return result
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        raise
 
 
 def getClipImageEmbedding(image: str) -> np.ndarray:
@@ -67,32 +86,31 @@ def getClipImageEmbedding(image: str) -> np.ndarray:
 
 def processImage(image: str):
     try:
-        time.sleep(0.5) 
+        time.sleep(0.05) 
 
         if DEBUG==True:    
             print(f"Processing: {image}")
         
         texts = extractTextFromImage(image)
-
-        if DEBUG==True:    
-            print("\n--- OCR Text Detected ---")
-            for i, text in enumerate(texts, 1):
-                print(f"{i}. {text}")
-        
-
         embedding = getClipImageEmbedding(image)
 
-        if DEBUG==True:    
-            print("\n--- CLIP Embedding ---")
-            print(embedding[:5])  
-            print(f"Embedding shape: {embedding.shape}")
+        "Generate a unique ID based on the image path, it handles the case where the same image is added multiple times,"
+        " it will have the same id and will be updated in qdrant instead of creating a new entry." 
+        
+        id=uuid.uuid5(namespace, image)  
+        point = PointStruct(
+            id=id,
+            vector=embedding.flatten().tolist(),
+            payload={
+                "image_path": image,
+                "ocr_text": texts
+            }
+        )
+        
+        qdrant.upsert(collection_name=collectionName, points=[point])
 
-        database.append({
-            "image_path": image,
-            "ocr_text": texts,
-            "clipEmbedding": embedding
-        })
-        print(f"Added {image} to database. Total: {len(database)}")
+        if DEBUG==True:  
+            print(f"Added {image} to qdrant.")
 
     except Exception as e:
         print(f"Error occurred: {e}")
@@ -101,7 +119,7 @@ def processImage(image: str):
 
 
 
-def searchThroughImages(query: str, database: list[dict], topK: int = 3) -> list[dict]:
+def searchThroughImages(query: str, topK: int = 3) -> list[dict]:
 
     try:
 
@@ -123,23 +141,18 @@ def searchThroughImages(query: str, database: list[dict], topK: int = 3) -> list
         text_features /= text_features.norm(dim=-1, keepdim=True)
         text_features = text_features.detach().cpu().numpy() 
         
-        # Loop through database. For each item, and calculate the similarity  
-      
-        scores=[]
-        for item in database:
-            score=np.dot(text_features[0], item["clipEmbedding"][0])
-            scores.append((score,item))
-
-        # Sort the results from highest to lowest score.
-        scores.sort(key=lambda x: x[0], reverse=True)
+        results = qdrant.search(
+            collection_name=collectionName,
+            query_vector=text_features.flatten().tolist(),
+            limit=topK
+        )
 
         # 7. Return the topK results.
-        return scores[:topK]
+        return results
 
     except Exception as e:
         print(f"Error occurred: {e}")
         raise
-
 
 
 class Handler(watchdog.events.PatternMatchingEventHandler):
@@ -163,22 +176,32 @@ class Handler(watchdog.events.PatternMatchingEventHandler):
     def on_deleted(self, event):
         global database
         try:
-            database = [item for item in database if item["image_path"] != event.src_path]
-            print(f"Removed {event.src_path} from database. Total images: {len(database)}")
+            qdrant.delete(
+                collection_name=collectionName,
+                points_selector=FilterSelector(
+                    filter=Filter(
+                        must=[
+                            FieldCondition(
+                                key="image_path",
+                                match=MatchValue(value=event.src_path)
+                            )
+                        ]
+                    )
+                )
+            )
+            if DEBUG == True:
+                print(f"Removed {event.src_path} from qdrant.")
+
         except Exception as e:
-                        print(f"Error occurred: {e}")
-                        raise
-
-
-  
-
+            print(f"Error occurred: {e}")
+            raise
 
 
 if __name__ == "__main__":
 
     path = "C:\\Users\\zadif\\Desktop\\VS CODE\\python\\CV\\CLIPXOCR\\images"
 
-    # processing already present images
+    # processing already present images if any
     files=os.listdir(path)
     imageFiles = [f for f in files if f.lower().endswith(('.jpg', '.png', '.jpeg'))]
 
